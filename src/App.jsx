e949f921dbe131aca478eefe7f56ba629f733fb5
Chris <chrislanzas89@outlook.com>
2026-02-17 15:14:16 -0500

    installed react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Heyyy</p>
+    </div>
+    
+  );
 }
 
 export default App;
